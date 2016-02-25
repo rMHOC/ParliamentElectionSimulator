f9cf2e01d8a1a365d0ba336d8bccbcfd6954e679
WadeMarshall <wmmnola@gmail.com>
2016-02-24 18:21:11 -0600

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Constiuency.py

---
+++ Voter.py
@@ -0,0 +1,27 @@
+class VoterGroup(Enum):
+#TODO: Uncomment this shit and implement all voting groups
+
+#    capitalist = {"socialView": 0, "economicView": 0, "interventionism":0}
+#    communter = {"socialView": 0, "economicView": 0, "interventionism":0}
+#    environmentalist = {"socialView": 0, "economicView": 0, "interventionism":0}
+#    ethnic_minority = {"socialView": 0, "economicView": 0, "interventionism":0}
+#    farmer = {"socialView": 0, "economicView": 0, "interventionism":0}
+#    liberal = {"socialView": 0, "economicView": 0, "interventionism":0}
+#    middle_class = {"socialView": 0, "economicView": 0, "interventionism":0}
+#    motorist = {"socialView": 0, "economicView": 0, "interventionism":0}
+#    patriot = {"socialView": 0, "economicView": 0, "interventionism":0}
+#    poor = {"socialView": 0, "economicView": 0, "interventionism":0}
+#    religious = {"socialView": 0, "economicView": 0, "interventionism":0}
+#    retired = {"socialView": 0, "economicView": 0, "interventionism":0}
+#    self_employed = {"socialView": 0, "economicView": 0, "interventionism":0}
+#    socialist = {"socialView": 0, "economicView": 0, "interventionism":0}
+#    state_employee = {"socialView": 0, "economicView": 0, "interventionism":0}
+#    trade_unionist = {"socialView": 0, "economicView": 0, "interventionism":0}
+#    wealthy = {"socialView": 0, "economicView": 0, "interventionism":0}
+#    youth = {"socialView": 0, "economicView": 0, "interventionism":0}
+
+Test_group1 = {"socialView": 0, "economicView": 0, "interventionism": 0}
+Test_group2 = {"socialView": 25, "economicView": 25, "interventionism": 25}
+Test_group3 = {"socialView": 50, "economicView": 50, "interventionism": 50}
+Test_group4 = {"socialView": 75, "economicView": 75, "interventionism": 75}
+Test_group5 = {"socialView": 100, "economicView": 100, "interventionism": 100}
