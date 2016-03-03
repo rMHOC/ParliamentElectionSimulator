8ce6fcae769831d704f1ec0a4fff52e74b51e515
jb567 <jb567@users.noreply.github..com>
2016-03-03 15:28:21 +1300

    fixed dict

--- Constiuency.py
+++ Constiuency.py
@@ -9,7 +9,7 @@ class Constituency(object):
         self.numofseats = []
 
     def populate(self):
-        population = {socialView: 0, economicView: 0, interventionism:0}
+        population = []
         for i in range(0,1000):
             population.append(Voter(i))
 
@@ -22,21 +22,21 @@ class Voter(object):
 
 
 class VoterGroup(Enum):
-    capitalist = {socialView: 0, economicView: 0, interventionism:0}
-    communter = {socialView: 0, economicView: 0, interventionism:0}
-    environmentalist = {socialView: 0, economicView: 0, interventionism:0}
-    ethnic_minority = {socialView: 0, economicView: 0, interventionism:0}
-    farmer = {socialView: 0, economicView: 0, interventionism:0}
-    liberal = {socialView: 0, economicView: 0, interventionism:0}
-    middle_class = {socialView: 0, economicView: 0, interventionism:0}
-    motorist = {socialView: 0, economicView: 0, interventionism:0}
-    patriot = {socialView: 0, economicView: 0, interventionism:0}
-    poor = {socialView: 0, economicView: 0, interventionism:0}
-    religious = {socialView: 0, economicView: 0, interventionism:0}
-    retired = {socialView: 0, economicView: 0, interventionism:0}
-    self_employed = {socialView: 0, economicView: 0, interventionism:0}
-    socialist = {socialView: 0, economicView: 0, interventionism:0}
-    state_employee = {socialView: 0, economicView: 0, interventionism:0}
-    trade_unionist = {socialView: 0, economicView: 0, interventionism:0}
-    wealthy = {socialView: 0, economicView: 0, interventionism:0}
-    youth = {socialView: 0, economicView: 0, interventionism:0}
+    capitalist = {"socialView": 0, "economicView": 0, "interventionism":0}
+    communter = {"socialView": 0, "economicView": 0, "interventionism":0}
+    environmentalist = {"socialView": 0, "economicView": 0, "interventionism":0}
+    ethnic_minority = {"socialView": 0, "economicView": 0, "interventionism":0}
+    farmer = {"socialView": 0, "economicView": 0, "interventionism":0}
+    liberal = {"socialView": 0, "economicView": 0, "interventionism":0}
+    middle_class = {"socialView": 0, "economicView": 0, "interventionism":0}
+    motorist = {"socialView": 0, "economicView": 0, "interventionism":0}
+    patriot = {"socialView": 0, "economicView": 0, "interventionism":0}
+    poor = {"socialView": 0, "economicView": 0, "interventionism":0}
+    religious = {"socialView": 0, "economicView": 0, "interventionism":0}
+    retired = {"socialView": 0, "economicView": 0, "interventionism":0}
+    self_employed = {"socialView": 0, "economicView": 0, "interventionism":0}
+    socialist = {"socialView": 0, "economicView": 0, "interventionism":0}
+    state_employee = {"socialView": 0, "economicView": 0, "interventionism":0}
+    trade_unionist = {"socialView": 0, "economicView": 0, "interventionism":0}
+    wealthy = {"socialView": 0, "economicView": 0, "interventionism":0}
+    youth = {"socialView": 0, "economicView": 0, "interventionism":0}
